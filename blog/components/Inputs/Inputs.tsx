32514bfd6511e2442b46e742d578778776b6f2f9
AlexandruIoan20 <117534646+AlexandruIoan20@users.noreply.github.com>
2023-07-17 12:07:03 +0300

    Interface for crud on todo list tasks

--- blog/components/Inputs/Inputs.tsx
+++ blog/components/Inputs/Inputs.tsx
@@ -1,4 +1,5 @@
 import { Todo as TodoInterface } from '../../models/interfaces/Todo'; 
+import { MdOutlineDownloadDone } from 'react-icons/md'; 
 import React from 'react';
 
 interface DefaultProps { 
@@ -10,25 +11,50 @@ interface DefaultProps {
   
   interface InputProps extends DefaultProps { 
     executeChange: (e: React.ChangeEvent<HTMLInputElement>) => void 
+    executeFunction?: () => void 
+  }
+
+  interface EditProps extends DefaultProps { 
+    executeChange: (e: React.ChangeEvent<HTMLInputElement>) => void 
+    executeEdit: () => void
   }
   
   interface TextAreaProps extends DefaultProps { 
     executeChange: (e: React.ChangeEvent<HTMLTextAreaElement>) => void
   }
   
-  const InputElement = ({ labelTitle, placeholder, required, value, executeChange }: InputProps) => { 
+  const InputElement = ({ labelTitle, placeholder, required, value, executeChange, executeFunction }: InputProps) => { 
     return ( 
       <>
         <label className = "input_label">
           <span className='mx-4 text-base my-0.5 font-medium font-satoshi'> { labelTitle } </span>
-          <input 
-            type="text" 
-            required = { required }
-            placeholder = { placeholder }
-            className='form_input'
-            value = { value }
-            onChange = { (e) => { executeChange(e)}}
-          /> 
+
+          { (executeFunction != undefined || executeFunction != null) && 
+            <div className = 'flex mr-6'>
+              <input 
+                type="text" 
+                required = { required }
+                placeholder = { placeholder }
+                className='form_input w-full'
+                value = { value }
+                onChange = { (e) => { executeChange(e)}}
+              /> 
+              <button type = 'button'
+                className = 'rounded-full bg-slate-300 px-3 hover:bg-slate-400 hover:scale-150 text-sm flex justify-center items-center transition-all duration-300'
+                onClick = { executeFunction }> + </button>
+            </div>
+          }
+
+          { (executeFunction == undefined || executeFunction == null) && 
+              <input 
+              type="text" 
+              required = { required }
+              placeholder = { placeholder }
+              className='form_input'
+              value = { value }
+              onChange = { (e) => { executeChange(e)}}
+            /> 
+          }
         </label>
       </>
     )
@@ -51,4 +77,27 @@ interface DefaultProps {
     )
 }
 
-export { TextAreaInput, InputElement}
+const EditInputElement = ({ labelTitle, placeholder, required, value, executeChange, executeEdit }: EditProps) => { 
+  return ( 
+    <div  >
+      <label className = "input_label">
+        <span className='mx-4 text-base my-0.5 font-medium font-satoshi'> { labelTitle } </span>
+        <div className = 'flex mr-2'>
+          <input 
+            type="text" 
+            required = { required }
+            placeholder = { placeholder }
+            className='form_input w-full'
+            value = { value }
+            onChange = { (e) => { executeChange(e)}}
+          /> 
+          <div className = 'bg-green-200 flex w-fit p-1 m-1 rounded-full hover:bg-green-300 cursor-pointer hover:scale-150 transition-all duration-300'>
+            <MdOutlineDownloadDone onClick = { executeEdit } width = { 24 } height = { 24 }  /> 
+          </div>
+        </div>
+      </label>
+    </div>
+  )
+}
+
+export { TextAreaInput, InputElement, EditInputElement}
--- blog/components/Todo/ListForm.tsx
+++ blog/components/Todo/ListForm.tsx
@@ -2,9 +2,10 @@
 
 import React, { useState, useEffect } from 'react'; 
 import {  Todo as  TodoInterface } from '../../models/interfaces/Todo'; 
-import { InputElement } from '../Inputs/Inputs';
+import { InputElement, EditInputElement } from '../Inputs/Inputs';
 import { BsPencil } from 'react-icons/bs'; 
 import { RiDeleteBin7Line } from 'react-icons/ri';
+import { MdOutlineDownloadDone } from 'react-icons/md'; 
 
 interface Props { 
     todo: Partial<TodoInterface> 
@@ -23,8 +24,8 @@ const FormItem = ({ text, onShowEditor, onDelete }: ItemProps) => {
         <article className = 'mx-4 flex justify-between bg-slate-300 px-2 py-1 shadow-md rounded-sm my-2'>
             <p> { text } </p>
             <div className = 'flex gap-x-2 items-center'>
-                <BsPencil className = 'hover:scale-150 transition-all duration-300' onClick = { onShowEditor } /> 
-                <RiDeleteBin7Line className = 'hover:scale-150 transition-all duration-300'  onClick = { onDelete } /> 
+                <BsPencil className = 'hover:scale-150 cursor-pointer transition-all duration-300' onClick = { onShowEditor } /> 
+                <RiDeleteBin7Line className = 'hover:scale-150 cursor-pointer transition-all duration-300'  onClick = { onDelete } /> 
             </div>
         </article>
     )
@@ -34,18 +35,29 @@ const ListForm = ({ todo, setTodo, listLength }: Props) => {
     const [ inputEls, setInputEls ] = useState <boolean []> ([]); //true for input open - false for input closed
     const [ newTodoText, setNewTodoText ] = useState <string> (""); 
     const [ editText, setEditText ] = useState <string> (""); 
+    const [ identifyDelete, setIdentifyDelete ] = useState <boolean> (false); 
     
     useEffect( () => {
         let updateEls = []; 
         const length = todo.list?.length || 0; 
-        for(let i = 0; i <= length; i++) { 
+        for(let i = 0; i < length; i++) { 
             updateEls.push(true); 
         }
 
         setInputEls(updateEls); 
      }, [todo.list])
 
-    const addElement = (text: string, index: number) => { 
+     useEffect( () => {
+        let updateInputs = []; 
+        for(let i = 0; i < inputEls.length - 1; i++) { 
+            updateInputs.push(false); 
+        }
+
+        setInputEls(updateInputs); 
+     }, [identifyDelete]); 
+
+    const addElement = (text: string) => { 
+        console.log("Add Element"); 
         if(newTodoText == "")  { 
             alert("Your task should not be empty."); 
         } else { 
@@ -55,22 +67,69 @@ const ListForm = ({ todo, setTodo, listLength }: Props) => {
             setTodo({ ...todo, list: newList }); 
     
             let updateInputs = inputEls; 
-            updateInputs[index] = false; 
-            inputEls.push(true); 
+            updateInputs.push(false); 
             setInputEls(updateInputs); 
-            console.log(todo); 
         }
     }
 
     const editElement = (text: string, index: number) => { 
-
+        console.log("Edit Element")
+        let list = []; 
+        if(text === "") {
+            alert("Your task should not be empty.")
+        } else { 
+            setEditText("") //reset the text editor
+            if(todo.list != undefined && todo.list[index] != undefined) { 
+                list = todo.list; 
+            } else { 
+                console.log("Something went wrong with the index."); 
+                return; 
+            }
+    
+            list[index].text = text 
+            setTodo({ ...todo, list: list }) //modify the text in the todo object 
+            
+            let updateInputs = inputEls; 
+            updateInputs[index] = false; 
+            setInputEls(updateInputs) //close the editor
+        }
     }
 
     const deleteElement = (index: number) => { 
+        console.log("Delete Element")
+        if(todo.list != undefined && todo.list[index] != undefined ) { //typescript check
+            setIdentifyDelete((x) => !x); 
 
+            let list = []; 
+            for(let i = 0; i < todo.list.length; i++) { 
+                if(i != index) { 
+                    list.push(todo.list[i]); 
+                } 
+            }
+
+            setTodo({ ...todo, list: list }) //delete the task from the list
+            setEditText(""); 
+
+            console.log({ list }); 
+        } else { 
+            alert("Cannot delete the item due to an error"); 
+            return;     
+        }
     }
 
     const showEditor = (index: number) => { 
+        console.log("Show Editor")
+        let textFromList = ""; 
+
+        if(todo.list != undefined && todo.list[index] != undefined) { 
+            textFromList = todo.list[index].text; 
+        }
+        else { 
+            console.log("Something went wrong"); 
+            return; 
+        }
+
+        setEditText(textFromList); 
         console.log({ index }); 
         let updateInputs = []; 
         for(let i = 0; i < inputEls.length; i++) { 
@@ -87,6 +146,7 @@ const ListForm = ({ todo, setTodo, listLength }: Props) => {
         <label className = 'input_label'>
             <>
                 <span className='mx-4 text-base my-0.5 font-medium font-satoshi'> Add new tasks here </span>
+                <pre> { JSON.stringify(inputEls)  }</pre>
                 { 
                     inputEls.map((x, index) => { 
                         const text = todo.list === undefined ? "" : todo.list[index] === undefined ? "" : todo.list[index].text 
@@ -94,17 +154,14 @@ const ListForm = ({ todo, setTodo, listLength }: Props) => {
                             <>
                                 { x ? 
                                     ( 
-                                        <>
-                                            <InputElement 
-                                                labelTitle='new Task'
-                                                placeholder = '...new task'
-                                                required = { false }
-                                                value = { newTodoText }
-                                                executeChange= { (e) => { setNewTodoText(e.target.value) }}
-                                            /> 
-            
-                                            <button onClick = { () => addElement(newTodoText, index) }> + </button>
-                                        </>
+                                        <EditInputElement 
+                                            labelTitle='Edit Task'
+                                            placeholder = '...new task'
+                                            required = { false }
+                                            value = { editText }
+                                            executeChange= { (e) => { setEditText(e.target.value); console.log({ editText }) }}
+                                            executeEdit = { () => { editElement(editText, index) }}
+                                        /> 
                                     ) : ( 
                                         <>
                                             <FormItem 
@@ -119,6 +176,17 @@ const ListForm = ({ todo, setTodo, listLength }: Props) => {
                         )
                     })
                 }
+
+                <div>
+                    <InputElement 
+                        labelTitle='new Task'
+                        placeholder = '...new task'
+                        required = { false }
+                        value = { newTodoText }
+                        executeChange= { (e) => { setNewTodoText(e.target.value) }}
+                        executeFunction = { () => addElement(newTodoText) }
+                    /> 
+                </div>
             </>
         </label>
     </>
