6e3c88370157310ee1a1b1f3b3e88402a440ea4f
AlexandruIoan20 <117534646+AlexandruIoan20@users.noreply.github.com>
2023-07-18 13:32:49 +0300

    Get the todo in its page

--- blog/app/users/[id]/todos/[todoid]/page.tsx
+++ blog/app/users/[id]/todos/[todoid]/page.tsx
@@ -1,9 +1,54 @@
-import React from 'react'
+'use client'; 
+import { useState, useEffect } from 'react'; 
+import { Todo as TodoInterface, initialTodo } from '../../../../../models/interfaces/Todo'; 
+import Todo from '@/components/Todo/Todo';
 
-const page = () => {
+import { usePathname } from 'next/navigation';
+import { useSession } from 'next-auth/react';
+
+const TodoPage = () => {
+  const pathName = usePathname(); 
+
+  const [ todo, setTodo ] = useState <Partial<TodoInterface>> (initialTodo); 
+  const [ loading, setLoading ] = useState <boolean> (false); 
+  const { data: session } = useSession(); 
+
+  const extractTodoId = () => { 
+    return pathName.split("/")[4]; 
+  }
+
+  const ID = extractTodoId(); // the todoid to do the backend request
+
+  useEffect ( () => { 
+    const getTodoData = async () => { 
+      setLoading(true); 
+
+      try { 
+        const response = await fetch(`/api/users/${session?.user?.id}/todos/${ID}`); 
+        const todoResponse = await response.json(); 
+
+        setTodo(todoResponse); 
+        console.log( { todoResponse }); 
+      } catch (err) { 
+        console.log(err); 
+      } finally { 
+        setLoading(false); 
+      }
+    }; 
+
+    getTodoData(); 
+  }, [])
   return (
-    <div>page</div>
+    <section>
+      { loading && 
+        <p> Loading... </p>
+      }
+
+      { !loading &&
+        <Todo />
+      }
+    </section>
   )
 }
 
-export default page
+export default TodoPage; 
